98ce37274c152c4f9260d5491f916f5086aca7b1
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-10-26 20:10:14 +0530

    Add files via upload

---
+++ add two number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,a,sum;
+	cout <<"enter the first number :";
+	cin>>n;
+	cout <<"enter the second number :";
+	cin>>a;
+	sum = a+n;
+	cout<<sum;
+	
+}
